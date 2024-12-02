d1a3f9787205b64839c421672d088915683abad0
Andrei Stanciu <andreistanciu.as@gmail.com>
2024-12-02 22:59:58 +0200

    Fix clippy issues and format

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,31 +1,38 @@
-use std::collections::{HashMap, HashSet};
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+use std::collections::HashMap;
 
 advent_of_code::solution!(1);
 
+#[must_use]
 pub fn part_one(input: &str) -> Option<u32> {
     let (left, right) = parse_input(input);
     Some(
         left.into_iter()
             .zip(right)
-            .map(|(nl, nr)| (nl - nr).abs() as u32)
+            .filter_map(|(nl, nr)| u32::try_from((nl - nr).abs()).ok())
             .sum(),
     )
 }
 
+#[must_use]
 pub fn part_two(input: &str) -> Option<u32> {
     let (left, right) = parse_input(input);
     let mut occurrences = HashMap::new();
-    for n in left.iter() {
+    for n in &left {
         occurrences.entry(*n).or_insert(0);
     }
-    for n in right.into_iter() {
+    for n in right {
         occurrences
             .entry(n)
             .and_modify(|occurence| *occurence += 1)
             .or_insert(1);
     }
 
-    Some(left.into_iter().map(|n| (n * occurrences[&n]) as u32).sum())
+    Some(
+        left.into_iter()
+            .filter_map(|n| u32::try_from(n * occurrences[&n]).ok())
+            .sum(),
+    )
 }
 
 fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
@@ -41,8 +48,8 @@ fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
         right.push(numbers[1]);
     }
 
-    left.sort();
-    right.sort();
+    left.sort_unstable();
+    right.sort_unstable();
 
     (left, right)
 }
